9110f73670bebf0d80c14630077a0310b3cb825c
unknown <viakeegan@gmail.com>
2022-01-18 18:26:27 -0500

    new_transaction

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,3 +3,24 @@ let db;
 
 // establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
 const request = indexedDB.open('budget_tracker', 1);
+
+// this event will emit if the database version changes
+request.onupgradeneeded = function(event) {
+    // save a reference to the database
+    const db = event.target.result;
+
+    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
+    db.createObjectStore('new_transaction', {autoIncrement: true });   
+};
+
+// upon a successful
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+
+    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
+    if (navigator.online) {
+        // todo: uploadTransacion();
+    }
+
+};
